4565eb9eddff488fc543acd3f2195d07fabc703d
G10v4nny23 <vi.riverazambrano@duocuc.cl>
2023-04-21 23:57:54 -0400

    Creacion Front

---
+++ src/app/Interfaces/producto.ts
@@ -0,0 +1,9 @@
+export interface Producto {
+    idProducto:number,
+    nombreProducto:string,
+    idCategoria:number,
+    descripcionCategoria:string,
+    stock:number,
+    precio:string,
+    esActivoProducto:number
+}
